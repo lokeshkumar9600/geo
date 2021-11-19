d58acd95f7ba6f402366dad6b9cf09629819c7d1
lokeshkumar9600 <lokesh960077152@gmail.com>
2021-11-19 08:12:55 +0530

    d

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.static(__dirname + "/public"))
-const PORT = 3000|| process.env.PORT
+const PORT = process.env.PORT ||5000;
 
 app.get("/",(req, res) => {
     res.send(__dirname+"/public/index.html")
